f9c46263ffaf08bb70879db227fa7119baf7f732
Stephen Johnston <52013605+stephendjohnston@users.noreply.github.com>
2023-02-22 16:29:45 -0400

    added index.html

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    // res.send('Yo!')
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 })
 
 app.listen(process.env.PORT || 3000, () => {
